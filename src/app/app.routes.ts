2b8b584e40420b7142c304cd13b1bc14ad85a832
theonlybenjamin <benja.villajuan@gmail.com>
2024-04-25 20:54:43 -0500

    guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
 import { TemplateFormComponent } from './forms/template-form/template-form.component';
 import { ReactiveFormComponent } from './forms/reactive-form/reactive-form.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {
-        path: 'template',
-        component: TemplateFormComponent
+        path: 'login',
+        component: LoginComponent,
+        canActivate: [
+            authGuard
+        ]
     },
     {
-        path: 'reactive',
-        component: ReactiveFormComponent
+        path: 'dashboard',
+        component: DashboardComponent
     }
 ];
